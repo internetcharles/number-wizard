7812b6908981db045b9abb46a4634a36645c573c
internetcharles <charlesodellsmith@gmail.com>
2020-05-16 11:53:29 -0700

    basic logic added, project uses basic 2d packages

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        UnityEngine.Debug.Log("Welcome to Number Wizard... I want to guess your freakin number.");
+        UnityEngine.Debug.Log("Pick a number!");
+        UnityEngine.Debug.Log($"Highest number is: {max}");
+        UnityEngine.Debug.Log($"Lowest number is: {min}");
+        UnityEngine.Debug.Log($"Tell me if your number is higher or lower than {guess}");
+        UnityEngine.Debug.Log("Push up = higher, Push Down = lower, Push Enter = Correct");
+        max = max + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            UnityEngine.Debug.Log($"Is your guess lower or higher than {guess}. Press up/down!");
+            min = guess;
+            guess = (max + min) / 2;
+            UnityEngine.Debug.Log(guess);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            UnityEngine.Debug.Log($"Is your guess lower or higher than {max}. Press up/down!");
+            max = guess;
+            guess = (max + min) / 2;
+            UnityEngine.Debug.Log(guess);
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            UnityEngine.Debug.Log("Return key was pressed.");
+        }
+
+
+    }
+}
